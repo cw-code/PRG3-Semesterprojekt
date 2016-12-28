a3d1c8d9d37d9b69e6253f4b1d9e4e6010562bf4
Christoph <chriswa1994@gmx.de>
2016-12-28 16:08:42 +0100

    Unit Tests

--- BlueRay-Kreisel/SourceCode/BlueRay-Kreisel/Test/Testpackage/CCustumer_Test.java
+++ BlueRay-Kreisel/SourceCode/BlueRay-Kreisel/Test/Testpackage/CCustumer_Test.java
@@ -1,9 +1,15 @@
 package Testpackage;
 
+import java.util.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import BRK.CCustomer;
 /**
  *
  * @author Christoph
  */
 public class CCustumer_Test {
     
+  // Hier kommen die Unit Tests
 }
