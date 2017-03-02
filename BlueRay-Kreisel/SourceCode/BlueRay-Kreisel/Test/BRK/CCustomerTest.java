fd2738c73c50778613f22c350746c996ef1e1fca
Christoph <chriswa1994@gmx.de>
2017-03-02 17:27:06 +0100

    Tests fuer Basket, Customer und Newsletter erstellt

--- BlueRay-Kreisel/SourceCode/BlueRay-Kreisel/Test/BRK/CCustomerTest.java
+++ BlueRay-Kreisel/SourceCode/BlueRay-Kreisel/Test/BRK/CCustomerTest.java
@@ -5,14 +5,13 @@
  */
 package BRK;
 
-import java.util.*;
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
-
-enum BRState 
-{ 
-    OWNER, LENT, RENT; 
-}
 
 /**
  *
@@ -20,9 +19,8 @@ enum BRState
  */
 public class CCustomerTest {
     
-    private CCustomer wagner;
-    private CBluRay minions;
-    private CBluRay HouseOfCards;
+    public CCustomerTest() {
+    }
     
     @BeforeClass
     public static void setUpClass() {
@@ -33,36 +31,54 @@ public class CCustomerTest {
     }
     
     @Before
-    public void setUp() 
-    {
-        wagner = new CCustomer("Wagner", "Christoph", "06.09.1994", "cw@web.de", "123456", myBRK);
-        minions = new CBluRay(wagner, BRState.OWNER , "Minions", "Kinder", 12);
-        HouseOfCards = new CBluRay(wagner, BRState.OWNER , "HouseOfCards", "Politik-Drama", 16);
+    public void setUp() {
     }
     
     @After
-    public void tearDown() 
-    {
+    public void tearDown() {
+    }
+
+    /**
+     * Test of gatherBR method, of class CCustomer.
+     */
+    @Test
+    public void testGatherBR() {
+        System.out.println("gatherBR");
+        BRK _BRK = new BRK();
+        CCustomer _Me = new CCustomer("Heinz", "Klaus", "19.02.1990", "klaus@gmx.de", "123456", _BRK);
+        CBluRay _BR1 = new CBluRay( _Me, BRState.OWNER, "Minions", "Kinder", 12);
+        CBluRay _BR2 = new CBluRay( _Me, BRState.OWNER, "Transformer", "Action", 12);
+        CBluRay _BR3 = new CBluRay( _Me, BRState.OWNER, "Scrubs", "Kinder", 12);
+        _Me.gatherBR(_BR1);
+        _Me.gatherBR(_BR2);
+        _Me.gatherBR(_BR3);
+        Assert.assertEquals("Minions", _Me.myBRList.get(0).getTitle());
+        Assert.assertEquals("Transformer", _Me.myBRList.get(1).getTitle());
+        Assert.assertEquals("Scrubs", _Me.myBRList.get(2).getTitle());
         
     }
 
+    /**
+     * Test of deleteBR method, of class CCustomer.
+     **/
     @Test
-    public void testGatherBluRay() 
-    {
-        wagner.gatherBluRay(minions);
-        wagner.gatherBluRay(HouseOfCards);
-        
-        wagner.printList(wagner.myBluRayList);
-        
-    }
-    
-    @Test
-    public void testdeleteBluRay()
-    {
-        wagner.deleteBluRay("HouseOfCards");
-        
-        wagner.printList(wagner.myBluRayList);
-        
+    public void testDeleteBR() {
+        System.out.println("deleteBR");
+        BRK _BRK = new BRK();
+        CCustomer _Me = new CCustomer("Heinz", "Klaus", "19.02.1990", "klaus@gmx.de", "123456", _BRK);
+        CBluRay _BR1 = new CBluRay( _Me, BRState.OWNER, "Minions", "Kinder", 12);
+        CBluRay _BR2 = new CBluRay( _Me, BRState.OWNER, "Transformer", "Action", 12);
+        CBluRay _BR3 = new CBluRay( _Me, BRState.OWNER, "Scrubs", "Kinder", 12);
+        _Me.gatherBR(_BR1);
+        _Me.gatherBR(_BR2);
+        _Me.gatherBR(_BR3);
+        Assert.assertEquals(3, _Me.myBRList.size());
+        _Me.deleteBR("Transformer");
+        Assert.assertEquals(2, _Me.myBRList.size());
+        _Me.deleteBR("Scrubs");
+        Assert.assertEquals(1, _Me.myBRList.size());
+        _Me.deleteBR("Scrubs");
+        Assert.assertEquals(1, _Me.myBRList.size());
     }
     
 }
